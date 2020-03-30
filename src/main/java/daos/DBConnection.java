071ab2521a9ffec6bf01e07c5df9b2757f2c288b
asconem <asconem@gmail.com>
2020-03-29 22:48:15 -0400

    Finished for now

---
+++ src/main/java/daos/DBConnection.java
@@ -0,0 +1,26 @@
+package daos;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static final String URL = "jdbc:mysql://localhost:3309/main";
+    public static final String USER = "root";
+    public static final String PASS = "password";
+
+    public static Connection getConnection() {
+        try {
+            DriverManager.registerDriver(new Driver());
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error connecting to the database", ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection connection = DBConnection.getConnection();
+    }
+}
